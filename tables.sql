c0739313601729bfecf3f5cd0b9e9f6683db41b2
hornd24 <david.horn689@gmail.com>
2017-12-01 13:53:31 -0800

    node

---
+++ tables.sql
@@ -0,0 +1,38 @@
+
+
+USE hot_Restaurant;
+
+CREATE TABLE table_list (
+  id INT NOT NULL AUTO_INCREMENT,
+  Table_Number INT(4) NULL,
+  Table_Status BOOLEAN DEFAULT True,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE customer_list(
+  id INT NOT NULL AUTO_INCREMENT,
+  Table_Number INT(4) NULL,
+  customer_name VARCHAR(45) NOT NULL ,
+  reservation_status VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO table_list 
+VALUES (1);
+
+INSERT INTO table_list 
+VALUES (2);
+
+INSERT INTO table_list 
+VALUES (3);
+
+
+INSERT INTO table_list 
+VALUES (4);
+
+
+INSERT INTO table_list 
+VALUES (5);
+
+
+
