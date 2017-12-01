5727e8fe5b18a71100fdd038b14b1880febc4e35
hornd24 <david.horn689@gmail.com>
2017-12-01 14:54:26 -0800

    sql

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host: "xq7t6tasopo9xxbs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+  
+    // Your username
+    user: "qclifbjefe47cqvv",
+  
+    // Your password
+    password: "dx43xlvi4o8yyuau",
+    database: "bsud9ij21mlz20o2"
+  });
 
-
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+   hi();
+   
+  });
+  function hi(){
+  var query = connection.query("SELECT * FROM table_list", function(err, res) {
+console.log(res)
+  })
+}
 var app = express();
 var PORT = process.env.PORT || 3000;
 
