67bfdcaf264af370df06acbf62d9ea7371f54a71
Tia Vasquez <tiavasquez3@gmail.com>
2017-12-04 10:50:22 -0800

    more changes

--- server.js
+++ server.js
@@ -2,15 +2,23 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
-
 var app = express();
 var PORT = process.env.PORT || 3000;
-
-
-
-
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-  
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/view", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
